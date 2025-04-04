5ac3f53e65892664138b3d5d51155efd0a53d746
Arteea <burlacka.artur@yandex.ru>
2025-04-04 10:00:22 +0300

    Service functions with business logic were added

--- tron_info_app/app/main.py
+++ tron_info_app/app/main.py
@@ -10,6 +10,8 @@ from . import models,schemas
 from .database import engine,get_database
 from sqlalchemy.orm import Session
 
+from .services import get_account_details,log_to_database
+
 from tronpy import Tron
 
 
@@ -24,35 +26,25 @@ app = FastAPI(
 
 
 @app.post("/get_wallet_info/", response_model = schemas.WalletInfoResponse)
-def get_wallet_info(request: schemas.WalletBase,
+def get_wallet_info(wallet: schemas.WalletBase,
                     db: Session = Depends(get_database)):
-    account = client.get_account(request.address)
-    account_energy = account.get("account_resource", {}).get("energy_usage", 0)
-    balance = client.get_account_balance(request.address)
-    bandwidth = client.get_bandwidth(request.address)
-
-    try:
-        db_request = models.WalletInfo(address = request.address)
-        db.add(db_request)
-        db.commit()
-        db.refresh(db_request)
-    except Exception as e:
-        return {'Exception': e}
-
-
-    return schemas.WalletInfoResponse(
-        address= request.address,
-        balance = balance,
-        bandwidth = bandwidth,
-        energy = account_energy)
+    
+    #Получаем данные кошелька
+    wallet_info = get_account_details(wallet=wallet,client=client)
+    #Записываем запрос в базу данных
+    log_to_database(wallet=wallet,database=db)
+    
+    return wallet_info
 
 
 
 @app.get("/get_list_of_wallets/",response_model = Page[schemas.WalletLogInfo])
 def get_list_of_wallets(db: Session = Depends(get_database),
                         page: int = Query(1,ge=0),
-                        size: int = Query(2)) -> list:
+                        size: int = Query(2)):
+
     query = db.query(models.WalletInfo).order_by(models.WalletInfo.time_stamp.desc())
+
     return paginate(query, params=Params(page=page, size=size))
 
 add_pagination(app)
--- tron_info_app/app/services.py
+++ tron_info_app/app/services.py
@@ -1,2 +1,46 @@
 from sqlalchemy.orm import Session
-from . import models,schemas
+from .schemas import WalletBase,WalletInfoResponse
+from typing import Annotated
+from fastapi import Depends,HTTPException
+from tronpy import Tron
+from .database import get_database
+from .models import WalletInfo
+from sqlalchemy.exc import SQLAlchemyError
+
+
+
+def verify_tron_client(client:Tron):
+    if not isinstance(client,Tron):
+        raise TypeError("Переданный аргумент не является объектом клиента Tron")
+    return client
+
+
+
+def get_account_details(wallet: WalletBase,
+                        client: Annotated[Tron, Depends(verify_tron_client)]
+                        ) -> WalletInfoResponse:
+    
+    account = client.get_account(wallet.address)
+    energy = account.get("account_resource", {}).get("energy_usage", 0)
+    balance = client.get_account_balance(wallet.address)
+    bandwidth = client.get_bandwidth(wallet.address)
+
+    return WalletInfoResponse(address=wallet.address,
+                              balance=balance,
+                              bandwidth=bandwidth,
+                              energy=energy)
+
+
+def log_to_database(wallet: WalletBase,database: Session = Depends(get_database)) ->bool:
+    try:
+        db_request = WalletInfo(address = wallet.address)
+        database.add(db_request)
+        database.commit()
+        database.refresh(db_request)
+    except SQLAlchemyError as e:
+        database.rollback()
+        raise HTTPException(status_code=500,detail={"message": f"Произошла ошибка базы данных: {e}"})
+    except Exception as e:
+        database.rollback()
+        raise HTTPException(status_code=500,detail={"message": f"Произошла ошибка на сервере: {e}"})
+    
